ccae8d7eb2aecc8013927f731124e9fedae1fc83
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-03 06:31:22 +0300

    pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,13 @@
-#!/usr/bin/python3
-"""Triangle of pascal"""
-
-
 def pascal_triangle(n):
-    """pascal"""
-    if n <= 0:
-        return []
-    pascal = [[1]]
-    for row_num in range(1, n):
-        row = [1]
-        for j in range(1, row_num):
-            element = pascal[row_num - 1][j - 1] + pascal[row_num - 1][j]
-            row.append(element)
-        row.append(1)
-        pascal.append(row)
-
-    return pascal
+    arr = []
+    for row in range(n):
+        arr.append([1])
+        for col in range(row + 1):
+            if (row > 0):
+                if (col == 0):
+                    pass
+                elif (col < (row / 2) or col == (row / 2)):
+                    arr[row].append(arr[row - 1][col - 1] + arr[row - 1][col])
+                else:
+                    arr[row].append(arr[row][row-col])
+    return arr
