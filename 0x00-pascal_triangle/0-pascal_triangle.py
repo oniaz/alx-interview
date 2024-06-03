d9c8f342d82da83d85f56c4154fbb33572c25a1b
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-03 06:35:51 +0300

    shebang + documentation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,21 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
 def pascal_triangle(n):
-    arr = []
+    """Generates Pascal's Triangle in the form of a list of lists,
+        where each inner list respresents a row int he triangle
+    """
+    pascal = []
     for row in range(n):
-        arr.append([1])
+        pascal.append([1])
         for col in range(row + 1):
             if (row > 0):
                 if (col == 0):
                     pass
                 elif (col < (row / 2) or col == (row / 2)):
-                    arr[row].append(arr[row - 1][col - 1] + arr[row - 1][col])
+                    pascal[row].append(
+                        pascal[row - 1][col - 1] + pascal[row - 1][col])
                 else:
-                    arr[row].append(arr[row][row-col])
-    return arr
+                    pascal[row].append(pascal[row][row-col])
+    return pascal
