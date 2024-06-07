9d0f6ce38579862283486bc3b1d07869ff4d15c7
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-07 14:20:13 +0300

    improve by using sets, looping over a copy of unvisited and adding missing_keys to the while condition

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,18 +3,18 @@
 
 
 def canUnlockAll(boxes):
-    """checks if all boxes can be unlocked or not. bruh"""
+    """Checks if all boxes can be unlocked or not."""
     change = True
-    missing_keys = list(range(1, len(boxes)))
-    unvisited = list(range(len(boxes)))
+    missing_keys = set(range(1, len(boxes)))
+    unvisited = set(range(len(boxes)))
 
-    while (change):
+    while change and missing_keys:
         change = False
-        for box in unvisited:
+        for box in unvisited.copy():
             if box not in missing_keys:
                 change = True
                 for new_key in boxes[box]:
                     if new_key in unvisited and new_key in missing_keys:
-                        missing_keys.pop(missing_keys.index(new_key))
-                unvisited.pop(unvisited.index(box))
-    return len(unvisited) == 0
+                        missing_keys.remove(new_key)
+                unvisited.remove(box)
+    return len(missing_keys) == 0
