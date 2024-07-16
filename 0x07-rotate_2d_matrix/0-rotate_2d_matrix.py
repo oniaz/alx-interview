3b06b049e0327f3d3c4d3b000bcb5bcd1ac02cf9
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-16 15:48:44 +0300

    replace inner with (start, end) as I did it from the start, why did i change it? frickin intrusive thoughts. trust the subconsciousnessf the  consciousness

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -14,9 +14,8 @@ def rotate_2d_matrix(matrix):
             s += 1
             e -= 1
         flag = True
-        inner = e - s
         k = 0
-        for j in range(inner):
+        for j in range(s, e):
             matrix[s][j], matrix[j][e] = matrix[j][e], matrix[s][j]
             matrix[s][j], matrix[e][e - k] = matrix[e][e - k], matrix[s][j]
             matrix[s][j], matrix[e - k][s] = matrix[e - k][s], matrix[s][j]
