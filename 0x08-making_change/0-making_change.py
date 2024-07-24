bf903de02a8c34564b1aab23a826b5f525540415
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-24 22:16:01 +0300

    0x08. Making Change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""The coin change problem: a classic problem from the domain of dynamic
+programming and greedy algorithms. The objective is to find the minimum number
+of coins required to make up a given total amount, given a list of coin
+denominations.
+"""
+
+
+def makeChange(coins, total):
+    """Given a pile of coins of different values, determine the fewest number
+    of coins needed to meet a given amount total.
+    coins: list of the values of the coins in your possession
+    Return: fewest number of coins needed to meet total
+    """
+    if total == 0:
+        return 0
+
+    amounts = [total + 1] * (total + 1)
+    amounts[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i >= coin:
+                amounts[i] = min(amounts[i], amounts[i - coin] + 1)
+
+    return amounts[total] if amounts[total] <= total else -1
