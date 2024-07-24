aa6fe8d3acef74bc48ef320a522546d4e7e32e80
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-24 22:20:08 +0300

    efficiency?

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,12 +15,12 @@ def makeChange(coins, total):
     if total == 0:
         return 0
 
-    amounts = [total + 1] * (total + 1)
-    amounts[0] = 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
 
     for i in range(1, total + 1):
         for coin in coins:
             if i >= coin:
-                amounts[i] = min(amounts[i], amounts[i - coin] + 1)
+                dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    return amounts[total] if amounts[total] <= total else -1
+    return dp[total] if dp[total] != float('inf') else -1
