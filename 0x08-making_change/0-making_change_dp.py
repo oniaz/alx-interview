09c7362246519bd3ce46ed9fa531feed6ee4eda5
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-25 22:21:36 +0300

    bitches lied and said they wanted the optimum solution; well dp way is the optimum solution to give a correct output you dumbfucks, tell me how using greedy algorithm with the answer your checker requires you'll get a correct answer for an input of [1, 3, 4], 6? you just wasted my time. fuck you fr. one more month and im done with this twattery. fuck you.

--- 0x08-making_change/0-making_change_dp.py
+++ 0x08-making_change/0-making_change_dp.py
@@ -8,19 +8,19 @@ denominations.
 
 def makeChange(coins, total):
     """Given a pile of coins of different values, determine the fewest number
-    of coins needed to meet a given amount total using greedy algorithm.
+    of coins needed to meet a given amount total using dynamic programming.
     coins: list of the values of the coins in your possession
     Return: fewest number of coins needed to meet total
     """
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)
-    count = 0
+    amounts = [total + 1] * (total + 1)
+    amounts[0] = 0
 
-    for coin in coins:
-        if total != 0:
-            count += total // coin
-            total -= (total // coin) * coin
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i >= coin:
+                amounts[i] = min(amounts[i], amounts[i - coin] + 1)
 
-    return count if total == 0 else -1
+    return amounts[total] if amounts[total] <= total else -1
