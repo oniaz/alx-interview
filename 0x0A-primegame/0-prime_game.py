3f67ad5fef88cd17d46ce8a0b1c6c7d37c50bde6
oniaz <32391049+oniaz@users.noreply.github.com>
2024-08-09 02:38:36 +0300

    changed peime numbers getter

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,25 +2,16 @@
 """ 0x0A. Prime Game """
 
 
-def is_prime(n):
-    """Check if a number is prime."""
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-
-def primes_up_to(y):
-    """Return a list of prime numbers up to y."""
-    return [num for num in range(2, y + 1) if is_prime(num)]
+def primes_up_to(limit):
+    """Generate list of primes up to limit"""
+    primeNumbers = []
+    sieveList = [True] * (limit + 1)
+    for potentialPrime in range(2, limit + 1):
+        if sieveList[potentialPrime]:
+            primeNumbers.append(potentialPrime)
+            for multiple in range(potentialPrime, limit + 1, potentialPrime):
+                sieveList[multiple] = False
+    return primeNumbers
 
 
 def isWinner(x, nums):
