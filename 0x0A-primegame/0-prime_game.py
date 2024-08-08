07f649434e8c4f0696b51c2999465c5ab6649e4c
oniaz <32391049+oniaz@users.noreply.github.com>
2024-08-09 02:25:38 +0300

    0x0A-primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,34 +1,45 @@
 #!/usr/bin/python3
-""" Prime Game """
+""" 0x0A. Prime Game """
 
 
-def generatePrimeNumbers(limit):
-    """Generate list of primes up to limit"""
-    primeNumbers = []
-    sieveList = [True] * (limit + 1)
-    for potentialPrime in range(2, limit + 1):
-        if sieveList[potentialPrime]:
-            primeNumbers.append(potentialPrime)
-            for multiple in range(potentialPrime, limit + 1, potentialPrime):
-                sieveList[multiple] = False
-    return primeNumbers
+def is_prime(n):
+    """Check if a number is prime."""
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 
-def isWinner(numRounds, roundValues):
-    """
-    Determine the winner of the game
-    """
-    if not numRounds or not roundValues:
+def primes_up_to(y):
+    """Return a list of prime numbers up to y."""
+    return [num for num in range(2, y + 1) if is_prime(num)]
+
+
+def isWinner(x, nums):
+    """Determine the winner of prime game"""
+    if not x or not nums:
         return None
-    mariaScore = benScore = 0
-    for i in range(numRounds):
-        primes = generatePrimeNumbers(roundValues[i])
-        if len(primes) % 2 == 0:
+    mariaScore = 0
+    benScore = 0
+    for i in range(x):
+        if nums[i] == 1:
             benScore += 1
         else:
-            mariaScore += 1
+            if (len(primes_up_to(nums[i]))) % 2 == 0:
+                benScore += 1
+            else:
+                mariaScore += 1
     if mariaScore > benScore:
-        return "Maria"
+        return 'Maria'
     elif benScore > mariaScore:
-        return "Ben"
-    return None
+        return 'Ben'
+    else:
+        return None
